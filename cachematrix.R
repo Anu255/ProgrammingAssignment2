7729c028eb6be3747fa8fa449e747951ccad69f9
Anu255 <525anurag.murali@gmail.com>
2017-12-20 09:08:35 +0530

    Add files via upload

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,40 @@
-## Put comments here that give an overall description of what your
-## functions do
 
-## Write a short comment describing this function
+-##This function function makeCacheMatrix gets a matrix as an input, sets the value of the matrix,
+  -#gets the value of the matrix, sets the inverse Matrix and gets the inverse Matrix. The matrix object
+  -#can cache its own object. 
+  -
+  -#<<- operator is used to assign a value to an object in an environment that is different 
+  -#from the current environment 
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setinv <- function(inverse) m <<- inverse
+  getinv <- function() m
+  list(set = set, get = get,
+       setinv = setinv,
+       getinvmean = getinvmean)
 }
 
 
-## Write a short comment describing this function
-
+## The function cacheSolve takes the output of the previous matrix makeCacheMatrix(matrix) as an 
+-# input and checks inverse matrix from makeCacheMatrix(matrix) has any value in it or not.
+  -# In case inverse matrix from makeCacheMatrix((matrix) is empty, it gets the original matrix data from 
+  -# and set the invertible  matrix by using the solve function.
+  -# In case inverse matrix from makeCacheMatrix, it returns a message  "Getting Cached Invertible Matrix" 
+  -#and the cached object
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-}
+  m <- x$getinv()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data, ...)
+  x$setinv(m)
+  m
+  }
